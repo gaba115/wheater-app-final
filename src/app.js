31e8ddac3c1263683444fff78d9dbf29be7f20d5
gaba115 <129797591+gaba115@users.noreply.github.com>
2023-04-27 21:14:30 +0200

    Forecast and some style

--- src/app.js
+++ src/app.js
@@ -13,6 +13,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["S", "M", "T", "W"];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+    <div class="weather-forecast-day">${day}</div>
+    <img src="https://openweathermap.org/img/wn/04n@2x.png" alt="Picture" />
+    <div class="weather-forecast-temp">
+      <span class="weather-forecast-temp-max">16</span>
+      <span class="weather-forecast-temp-min">11</span>
+    </div>
+  </div>
+`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showTemp(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#city");
@@ -73,8 +97,8 @@ form.addEventListener("submit", searchForm);
 
 let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click", showFahrenheit);
-
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", showCelsius);
 
 search("New York");
+displayForecast();
